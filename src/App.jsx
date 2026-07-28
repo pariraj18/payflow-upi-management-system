244bd34cc2796eac671976b6374696ba73ded9d9
pariraj18 <pariraj18092006@gmal.com>
2026-07-28 21:33:44 +0530

    adding files and folders in frontend

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/Navbar";
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <h1>Welcome to PayFlow</h1>
+      <p>UPI Management Simulator</p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+function Navbar() {
+  return (
+    <nav>
+      <h2>PayFlow</h2>
+
+      <div>
+        <a href="/">Home</a>
+        <a href="/send-money">Send Money</a>
+        <a href="/transactions">Transactions</a>
+        <a href="/profile">Profile</a>
+      </div>
+    </nav>
+  );
+}
+
+export default Navbar;
