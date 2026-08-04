e7870f7fae40af368e5f2617004a5202dba607eb
pariraj18 <pariraj18092006@gmail.com>
2026-08-04 22:06:50 +0530

    vite.config.js changed

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+
+    VitePWA({
+      registerType: 'autoUpdate',
+
+      manifest: {
+        name: 'PayFlow',
+        short_name: 'PayFlow',
+        description: 'Offline Payment Simulator',
+        theme_color: '#172554',
+        background_color: '#172554',
+        display: 'standalone',
+        start_url: '/',
+      }
+    })
+  ]
 })
